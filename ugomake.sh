f0a13009750132bc0598770063ce2cd6cf198043
Ugo Di Girolamo <ugo@compass.com>
2020-03-17 12:21:40 -0400

    ugomake.sh fixes

--- ugomake.sh
+++ ugomake.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 set -eux
 
-export rev="0.9"
+export rev="ugo-0.10"
+eval `aws ecr get-login --profile backend-services --region=us-east-1 --no-include-email`
 make -f Makefile.release release
 make -f Makefile.release docker
-docker tag coredns/coredns:coredns-amd64 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:ugo-$rev
-docker push 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:ugo-$rev
+docker tag coredns/coredns:coredns-amd64 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
+docker push 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
