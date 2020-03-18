3bdb81266938b20953bfebd3a6156e3e40f9e4d3
Ugo Di Girolamo <ugo@compass.com>
2020-03-18 14:52:35 -0400

    Compass release, sent to staging-cluster

--- ugomake.sh
+++ ugomake.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 set -eux
 
-export rev="ugo-0.10"
-eval `aws ecr get-login --profile backend-services --region=us-east-1 --no-include-email`
+export rev="compass-1.6.7.1"
+#eval `aws ecr get-login --profile backend-services --region=us-east-1 --no-include-email`
+eval `aws ecr get-login --profile staging-cluster --region=us-east-1 --no-include-email`
 make -f Makefile.release release
 make -f Makefile.release docker
-docker tag coredns/coredns:coredns-amd64 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
-docker push 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
+#docker tag coredns/coredns:coredns-amd64 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
+#docker push 667043388539.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
+docker tag coredns/coredns:coredns-amd64 961350931373.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
+docker push 961350931373.dkr.ecr.us-east-1.amazonaws.com/coredns:$rev
